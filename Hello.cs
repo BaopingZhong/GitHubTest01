f7d4a23e3f000ec7f752b5e8e7f350d4a6e9d5d4
BaopingZhong <88601571+BaopingZhong@users.noreply.github.com>
2021-08-08 12:05:07 +0800

    Create Hello.cs
    第一次提交的GitHub的cSharp文件

---
+++ Hello.cs
@@ -0,0 +1,16 @@
+using System;
+public class Hello
+{   
+    public int x = 100;
+    public static int y = 200;
+    
+    public static void Main()
+    {
+        Console.WriteLine("HELLO WORLD ZBP");
+    }
+    
+    public static void Fly()
+    {
+        Console.WriteLine("I am Flying");
+    }
+}
